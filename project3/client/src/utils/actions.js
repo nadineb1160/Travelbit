f93de106e7fbcb6b8c03e64080b82c37080074f1
nadineb <nadineb@umich.edu>
2020-05-14 22:51:07 -0700

    [add] actions

---
+++ project3/client/src/utils/actions.js
@@ -0,0 +1,8 @@
+export const ADD_COUNTRY = "ADD_COUNTRY";
+export const UPDATE_COUNTRY = "UPDATE_COUNTRY";
+export const REMOVE_COUNTRY = "REMOVE_COUNTRY";
+export const LOADING = "LOADING";
+
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+export const UPDATE_FAVORITES = "UPDATE_FAVORITES";
