20c8a9e86abf6688c085727200d202c0aa06fd4f
nadineb <nadineb@umich.edu>
2020-05-14 22:44:09 -0700

    [update] server.js

--- project3/server.js
+++ project3/server.js
@@ -1,24 +1,33 @@
 // Dependencies
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 var PORT = process.env.PORT || 3000;
+
+// Define middleware to parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//   }
 
 // Static Directory
 app.use(express.static("public"));
 
 var db = require("./models");
 
-// Parse Request Body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // Routes
+// require("./routes/api-country-routes")(app);
+app.use(routes);
 
 // Sync Sequelize Models
 db.sequelize.sync().then(function () {
     // Starting Express App
     app.listen(PORT, function () {
-        console.log("Server listening at localhost:" + PORT);
+        console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
     })
 })
