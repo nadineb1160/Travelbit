6fe62ae307ce4c33e7d411646f7f9cafd34114ed
nadineb <nadineb@umich.edu>
2020-05-14 22:46:10 -0700

    [add] user context provider

---
+++ project3/client/src/state/UserContext.js
@@ -0,0 +1,34 @@
+import React, {useContext, useState} from "react";
+
+const UserContext = React.createContext({
+  name: "",
+  getUserToken: () => {},
+  login: () => {}
+});
+
+function getUserToken() {
+  return "SampleToken123";
+}
+function getLogin() {
+  return "SampleToken123";
+}
+
+export const UserProvider = ({children}) => {
+
+    const [user, setUser] = useState({
+        name: "Bob",
+        getUserToken: getUserToken,
+        login: getLogin
+    });
+
+      
+    return (
+        <UserContext.Provider value={user}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export const useUserContext = () => useContext(UserContext); 
+// import {useUserContext} from "../UserContext.js"
+// const {name, getUserToken} = useUserContext();
