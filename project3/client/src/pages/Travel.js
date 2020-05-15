e48ecef859dfce24e02dc1573f9b0177b0372311
nadineb <nadineb@umich.edu>
2020-05-14 22:49:21 -0700

    [add] travel page

---
+++ project3/client/src/pages/Travel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardContainer from '../components/CardContainer';
+
+const Travel = () => {
+    return (
+        <div>
+            <CardContainer />
+        </div>
+    )
+}
+
+export default Travel;
