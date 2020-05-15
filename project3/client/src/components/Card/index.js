e60b5d486398cc629a897fea0cd1597cc16a2a97
nadineb <nadineb@umich.edu>
2020-05-14 22:55:00 -0700

    [add] card and container

---
+++ project3/client/src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+
+function Card({ image }) {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Card;
---
+++ project3/client/src/components/CardContainer/index.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import Card from '../Card';
+
+const CardContainer = () => {
+
+    return (
+       <div>
+
+       </div>
+    )
+}
+
+export default CardContainer
