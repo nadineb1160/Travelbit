38a96bd4a34ce975dcc21050f4ac4e1241bb89ad
nadineb <nadineb@umich.edu>
2020-05-18 14:06:42 -0700

    [update] home

--- project3/client/src/pages/Home.js
+++ project3/client/src/pages/Home.js
@@ -3,16 +3,13 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <div className="mt-4">
+            {/* <div className="mt-4">
                 <a href="#" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-teal-500 text-sm text-white uppercase tracking-wider font-semibold">Explore</a>
             </div>
             <h1 className="mt-6 text-2xl font-bold text-gray-900" id="marker"><span className="text-teal-500">Travel </span>back in time</h1>
-            <div className="flex">
-                {/* <img className="mt-6 shadow-xl" src="https://cdn.pixabay.com/photo/2018/06/27/17/48/fantasy-3502188_960_720.jpg" alt="Woods"/>
-                <img className="mt-6 shadow-xl" src="https://cdn.pixabay.com/photo/2018/07/08/18/48/luggage-3524502_960_720.jpg" alt="Woods"/> */}
-            </div>
+             */}
+            <img class="w-full" src="https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067_1280.png" alt="moutain"/>
             {/* <img class="mt-6 shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3BrOUS69Iri4zvzdjA844B57PyLFG1WfPNlJumjOsaPCFz3nC&usqp=CAU" alt="Woods"/> */}
-            {/* <img className="mt-6 shadow-xl" src="https://cdn.pixabay.com/photo/2017/10/11/22/16/trees-2842682_960_720.jpg" alt="Woods"/> */}
         </div>
     )
 }
