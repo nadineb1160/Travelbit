ee8de529f626c4fe83d91387a3c4aeb970bd1f3b
nadineb <nadineb@umich.edu>
2020-05-18 14:08:34 -0700

    [add] to footer

--- project3/client/src/components/Footer/index.js
+++ project3/client/src/components/Footer/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="">
-      <span>Travel Log</span>
+
+    <footer className="w-full pin-b text-center border-t border-grey p-4">
+      <span className="">Travel Log</span>
     </footer>
+
   );
 }
 
