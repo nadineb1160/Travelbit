6ef49011876fcef678a1929eeff2d4331b998c5e
nadineb <nadineb@umich.edu>
2020-05-18 12:51:12 -0700

    [add] Card

--- project3/client/src/components/Card/index.js
+++ project3/client/src/components/Card/index.js
@@ -1,10 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 function Card({ image }) {
 
     return (
-        <div>
-            
+        <div className="bg-white border rounded-lg overflow-hidden m-4 max-w-sm shadow-lg">
+            <div className="relative bg-red-500 h-48">
+                <img src="https://cdn.pixabay.com/photo/2018/08/08/18/49/berlin-cathedral-3592874_1280.jpg" alt="image" className="absolute h-full w-full object-cover"/>
+            </div>
+            <div className="px-6 py-4">
+                <h3 className="text-gray-900">Location</h3>
+            </div>
         </div>
     );
 }
