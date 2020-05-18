b31e4b7ed14b315b5b35b4481476c0a0639955d5
nadineb <nadineb@umich.edu>
2020-05-18 12:51:31 -0700

    [add] Card Container

--- project3/client/src/components/CardContainer/index.js
+++ project3/client/src/components/CardContainer/index.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from "react";
 import Card from '../Card';
 
+
 const CardContainer = () => {
 
     return (
-       <div>
-
+       <div className="w-1/3 flex p-6">
+           <Card/>
+           <Card/>
        </div>
     )
 }
