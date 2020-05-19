15aecac68caf5c7db60f71ac1c932ba535c86699
nadineb <nadineb@umich.edu>
2020-05-18 22:40:01 -0700

    [add] user and cty routes

--- project3/routes/api/index.js
+++ project3/routes/api/index.js
@@ -1,11 +1,16 @@
 const router = require("express"). Router();
+const userRoutes = require("./user");
 const countryRoutes = require("./country");
+const cityRoutes = require("./city");
+
+// User routes
+router.use("/user", userRoutes);
 
 // Country routes
 router.use("/country", countryRoutes);
 
 // City routes
-
+router.use("/city", cityRoutes);
 
 
 module.exports = router;
