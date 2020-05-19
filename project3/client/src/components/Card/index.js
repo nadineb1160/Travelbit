d25b63f79b55a7390868e9c2d28865dd9211c916
nadineb <nadineb@umich.edu>
2020-05-18 22:36:17 -0700

    [update] card

--- project3/client/src/components/Card/index.js
+++ project3/client/src/components/Card/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-function Card({ image }) {
+function Card({ card }) {
 
     return (
-        <div className="bg-white border rounded-lg overflow-hidden m-4 max-w-sm shadow-lg">
-            <div className="relative bg-red-500 h-48">
-                <img src="https://cdn.pixabay.com/photo/2018/08/08/18/49/berlin-cathedral-3592874_1280.jpg" alt="image" className="absolute h-full w-full object-cover"/>
-            </div>
-            <div className="px-6 py-4">
-                <h3 className="text-gray-900">Location</h3>
+        <div className="block">
+            <div className="flex flex-col">
+
+                <div className="bg-white border rounded-lg overflow-hidden m-4 shadow-lg">
+                    <div className="relative h-48">
+                        <img src="https://cdn.pixabay.com/photo/2018/08/08/18/49/berlin-cathedral-3592874_1280.jpg" alt="bg" className="absolute h-full w-full object-cover"/>
+                    </div>
+                    <div className="px-6 py-4">
+                        <h3 className="text-gray-900">Location</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
