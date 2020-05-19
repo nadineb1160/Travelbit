4bf181df06164863d138803bb47fda2d4335daa4
nadineb <nadineb@umich.edu>
2020-05-18 22:35:18 -0700

    [add] firebase config

---
+++ project3/client/src/base.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const fireBaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    senderId: process.env.REACT_APP_SENDER_ID
+});
+
+export default fireBaseApp;
