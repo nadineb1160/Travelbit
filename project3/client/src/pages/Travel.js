8a8400a3206b259b9240bdfee76f17aa7fcef4a8
nadineb <nadineb@umich.edu>
2020-05-18 22:37:35 -0700

    [add] state to travel page

--- project3/client/src/pages/Travel.js
+++ project3/client/src/pages/Travel.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CardContainer from '../components/CardContainer';
+import {useUserContext} from "../state/UserContext.js"
+import API from "../utils/API";
 
 const Travel = () => {
+    const {name, getUserToken} = useUserContext();
+
+    const [displayCards, setDisplayCards] = useState([]);
+    const [selected, setSelected] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    // useEffect(() => {
+    //     setLoading(true);
+    //     API.getCountries().then((countries) => {
+    //         setDisplayCards(countries);
+    //         setLoading(false);
+    //     })
+    // }, []);
+
+
+
     return (
         <div>
-            <CardContainer />
+            <h2></h2>
+            <CardContainer 
+            cards={displayCards}
+            />
         </div>
     )
 }
