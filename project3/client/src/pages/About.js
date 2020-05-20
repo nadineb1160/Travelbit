fe5bc98baea4d8fd8fbacf3d244a444f98faccbe
nadineb <nadineb@umich.edu>
2020-05-19 21:48:09 -0700

    [add] to about

--- project3/client/src/pages/About.js
+++ project3/client/src/pages/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-           
+        <div className="text-center mt-5">
+           <h2>This is TravelBit. A Storage for all your travels back in time to remember your past adventures for years to come</h2>
         </div>
     )
 }
