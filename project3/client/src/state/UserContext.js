dba2559f53b5d53fbd8677cf65210a8851c68b05
nadineb <nadineb@umich.edu>
2020-05-19 18:50:58 -0700

    [update] userContext

--- project3/client/src/state/UserContext.js
+++ project3/client/src/state/UserContext.js
@@ -1,12 +1,14 @@
-import React, {useContext, useState, useEffect} from "react";
-import app from "../base.js"
+import React, { useContext, useState, useEffect } from "react";
+import { auth, generateUserDocument } from "../firebase";
 
 const UserContext = React.createContext({
-  id: 0,
-  name: "",
-  email: "",
-  getUserToken: () => {},
-  login: () => {}
+  // id: 0,
+  // name: "",
+  // email: "",
+  // getUserToken: () => { },
+  // login: () => { }
+
+  user: null
 });
 
 function getUserToken() {
@@ -18,25 +20,33 @@ function getLogin() {
 
 export const UserProvider = ({ children }) => {
 
-    const [user, setUser] = useState({
-        id: 0,
-        name: "Bob",
-        email: "bob@bob.com",
-        getUserToken: getUserToken,
-        login: getLogin
-    });
+  const [user, setUser] = useState({
+    // id: 0,
+    // name: "Bob",
+    // email: "bob@bob.com",
+    // getUserToken: getUserToken,
+    // login: getLogin
+    user: null
+  });
 
-    // useEffect(() => {
-    //   app.auth().onAuthStateChanged(setUser);
-    // }, []);
-      
-    return (
-        <UserContext.Provider value={user}>
-            {children}
-        </UserContext.Provider>
-    );
+  // useEffect(() => {
+  //   app.auth().onAuthStateChanged(setUser);
+  // }, []);
+
+  useEffect(async () => {
+    auth.onAuthStateChanged(async userAuth => {
+      const user = await generateUserDocument(userAuth);
+      setUser({ user: user});
+    });
+  }, [])
+
+  return (
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
-export const useUserContext = () => useContext(UserContext); 
+export const useUserContext = () => useContext(UserContext);
 // import {useUserContext} from "../UserContext.js"
 // const {name, getUserToken} = useUserContext();
