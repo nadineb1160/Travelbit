5cba10bd98addf359609d83e90d2ea73fd04b893
nadineb <nadineb@umich.edu>
2020-05-19 21:48:53 -0700

    [add] move tailwind footer

---
+++ project3/client/src/components/Footer/index_tailwind.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+
+    <footer className="w-full pin-b text-center border-t border-grey p-4">
+      <span className="">Travel Log</span>
+    </footer>
+
+  );
+}
+
+export default Footer;
