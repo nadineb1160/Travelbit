01ca5c3b9ea71429039d92bbeca883f4e06b1f92
nadineb <nadineb@umich.edu>
2020-05-19 18:51:47 -0700

    [add] firebase

---
+++ project3/client/src/firebase.js
@@ -0,0 +1,57 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { UserProvider } from "./state/UserContext";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBI_TADYflj_M4qeZD0XslBFWqo46zpYYg",
+    authDomain: "travelbit-ce02a.firebaseapp.com",
+    databaseURL: "https://travelbit-ce02a.firebaseio.com",
+    projectId: "travelbit-ce02a",
+    storageBucket: "travelbit-ce02a.appspot.com",
+    senderId: "830021166356",
+    appId: "1:830021166356:web:ab84a29578bb2e476a8e01"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+
+export const generateUserDocument = async(user, additionalData) => {
+    if (!user) return;
+    const userRef = firestore.doc(`users/${user.uid}`);
+    const snapshot = await userRef.get();
+    if (!snapshot.exists) {
+        const { email, displayName, photoUrl } = user;
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                photoUrl,
+                ...additionalData
+                
+            });
+        } catch (error) {
+            console.error("Error creating user document", error)
+        }
+    }
+    return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+    if (!uid) return null;
+    try {
+        const userDocument = await firestore.doc(`user/${uid}`)
+        return {
+            uid,
+            ...userDocument.data()
+        };
+    } catch (error) {
+        console.log("Error fetching user", error);
+    }
+}
+
+
+// export default fireBaseApp;
