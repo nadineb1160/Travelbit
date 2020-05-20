1568aa0c77d1609b935f2cc2dacd540e7464e050
nadineb <nadineb@umich.edu>
2020-05-19 18:52:32 -0700

    [update] router with authentication

--- project3/client/src/components/Router/index.js
+++ project3/client/src/components/Router/index.js
@@ -2,28 +2,49 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from "../../pages/About";
 import Home from "../../pages/Home";
-import Login from "../../pages/Login";
+import SignUp from "../SignUp";
+import SignIn from "../SignIn";
+import PasswordReset from "../PasswordReset";
 import Profile from "../../pages/Profile";
 import Travel from "../../pages/Travel";
 import Navbar from "../Navbar/index";
 import Footer from "../Footer/index";
 import Wrapper from "../Wrapper/index";
+import {useUserContext} from "../../state/UserContext.js"
+
+function AuthenticatedRoutes() {
+    return (
+        
+        <Switch>
+            <Route exact path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
+            <Route exact path="/passwordReset" component={PasswordReset} />
+        </Switch>
+    )
+}
 
 function Router() {
+    const {user} = useUserContext();
+
+
     return (
         <BrowserRouter>
             <Navbar />
             <Wrapper>
+                {user ?
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/travel" component={Travel} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/profile" component={Profile} />
-                    <Route exact path="/login" component={Login} />
+                    {/* <Route exact path="/signup" component={SignUp} />
+                    <Route exact path="/signin" component={SignIn} /> */}
                     {/* <Route component={NoMatch} /> */}
                 </Switch>
+                : <AuthenticatedRoutes/>
+                }
             </Wrapper>
-            {/* <Footer /> */}
+            <Footer />
         </BrowserRouter>
     );
 }
