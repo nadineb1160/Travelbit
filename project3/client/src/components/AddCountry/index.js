88b753a52b848298b0f01fc961190493dd3e297e
nadineb <nadineb@umich.edu>
2020-05-20 14:41:49 -0700

    [add] country form

---
+++ project3/client/src/components/AddCountry/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function AddCountry() {
+
+    const handleAdd = (event) => {
+        event.preventDefault();
+        console.log("hello")
+
+    }
+
+    return (
+        <div>
+
+            <div className="flex justify-center m-6">
+                <form className="w-full max-w-sm">
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="country">
+                                Country
+                            </label>
+                        </div>
+                        <div className="md:w-2/3">
+                            <input className="bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500" id="country" type="text" placeholder="Germany" />
+                        </div>
+                    </div>
+                    <div className="md:flex md:items-center mb-6">
+                        
+                        <div className="md:w-1/3">
+                            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="continent">
+                                Continent
+                            </label>
+                        </div>
+                        <div className="md:w-2/3">
+                            <select className="bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500" id="continent">
+                                <option>Africa</option>
+                                <option>Antarctica</option>
+                                <option>Australia</option>
+                                <option>Asia</option>
+                                <option>Europe</option>
+                                <option>North America</option>
+                                <option>South America</option>
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="md:flex md:items-center">
+                        <div className="md:w-1/3"></div>
+                        <div className="md:w-2/3">
+                            <button onClick={handleAdd} className="shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                                Add +
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default AddCountry;
