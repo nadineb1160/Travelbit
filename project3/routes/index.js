6c42868bcdd9ad3cd4c88f35f6d434a7b9e143aa
nadineb <nadineb@umich.edu>
2020-05-19 18:50:25 -0700

    [update] build -> public

--- project3/routes/index.js
+++ project3/routes/index.js
@@ -8,7 +8,7 @@ router.use("/api", apiRoutes);
 // If no API routes hit -> send React app
 router.use(function(req, res) {
     // build or public?
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    res.sendFile(path.join(__dirname, "../client/public/index.html"))
 });
 
 module.exports = router;
