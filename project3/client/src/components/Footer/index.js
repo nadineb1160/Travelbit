5ce01cc26af6a291b8c3198067b6447e0b31ddcc
nadineb <nadineb@umich.edu>
2020-05-19 21:48:20 -0700

    [add] sticky footer

--- project3/client/src/components/Footer/index.js
+++ project3/client/src/components/Footer/index.js
@@ -3,9 +3,15 @@ import React from "react";
 function Footer() {
   return (
 
-    <footer className="w-full pin-b text-center border-t border-grey p-4">
-      <span className="">Travel Log</span>
-    </footer>
+    <nav className="navbar fixed-bottom navbar-dark bg-dark">
+    
+      <a className="navbar-brand" href="#">Made with love</a>
+      <svg className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clip-rule="evenodd" />
+      </svg>
+   
+
+    </nav>
 
   );
 }
