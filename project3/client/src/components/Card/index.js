ce6ea9655d66c64a4b8599a87d45a68074c7fbd0
nadineb <nadineb@umich.edu>
2020-05-19 18:48:26 -0700

    [add] bootstrap card

--- project3/client/src/components/Card/index.js
+++ project3/client/src/components/Card/index.js
@@ -3,16 +3,11 @@ import React from "react";
 function Card({ card }) {
 
     return (
-        <div className="block">
-            <div className="flex flex-col">
-
-                <div className="bg-white border rounded-lg overflow-hidden m-4 shadow-lg">
-                    <div className="relative h-48">
-                        <img src="https://cdn.pixabay.com/photo/2018/08/08/18/49/berlin-cathedral-3592874_1280.jpg" alt="bg" className="absolute h-full w-full object-cover"/>
-                    </div>
-                    <div className="px-6 py-4">
-                        <h3 className="text-gray-900">Location</h3>
-                    </div>
+        <div>
+            <div className="card" style={{width: "20rem"}}>
+                <img src="https://cdn.pixabay.com/photo/2018/08/08/18/49/berlin-cathedral-3592874_1280.jpg" className="card-img-top p-6 pt-8" alt="..." />
+                <div className="card-body pt-1">
+                    <p className="card-text text-center marker" style={{"fontSize": "3rem"}}>Berlin</p>
                 </div>
             </div>
         </div>
