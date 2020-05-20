4496250dc7b8fb3be53f8919426997f032c60cb9
nadineb <nadineb@umich.edu>
2020-05-20 12:16:31 -0700

    [rmv] photoURL

--- project3/client/src/firebase.js
+++ project3/client/src/firebase.js
@@ -18,18 +18,23 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // user
+    }
+})
+
 
 export const generateUserDocument = async(user, additionalData) => {
     if (!user) return;
     const userRef = firestore.doc(`users/${user.uid}`);
     const snapshot = await userRef.get();
     if (!snapshot.exists) {
-        const { email, displayName, photoUrl } = user;
+        const { email, displayName } = user;
         try {
             await userRef.set({
                 displayName,
                 email,
-                photoUrl,
                 ...additionalData
                 
             });
@@ -43,13 +48,14 @@ export const generateUserDocument = async(user, additionalData) => {
 const getUserDocument = async uid => {
     if (!uid) return null;
     try {
-        const userDocument = await firestore.doc(`user/${uid}`)
-        return {
-            uid,
-            ...userDocument.data()
-        };
+      const userDocument = await firestore.doc(`users/${uid}`).get();
+  
+      return {
+        uid,
+        ...userDocument.data()
+      };
     } catch (error) {
-        console.log("Error fetching user", error);
+      console.error("Error fetching user", error);
     }
 }
 
