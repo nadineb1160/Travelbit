30a487325c74fbef59465c11a812bd2b4b54e7a0
nadineb <nadineb@umich.edu>
2020-05-19 18:54:41 -0700

    [update]

--- project3/client/src/pages/Home.js
+++ project3/client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
     return (
@@ -8,7 +8,7 @@ const Home = () => {
             </div>
             <h1 className="mt-6 text-2xl font-bold text-gray-900" id="marker"><span className="text-teal-500">Travel </span>back in time</h1>
              */}
-            <img class="w-full" src="https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067_1280.png" alt="moutain"/>
+            <img className="w-full" src="https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067_1280.png" alt="moutain"/>
             {/* <img class="mt-6 shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3BrOUS69Iri4zvzdjA844B57PyLFG1WfPNlJumjOsaPCFz3nC&usqp=CAU" alt="Woods"/> */}
         </div>
     )
