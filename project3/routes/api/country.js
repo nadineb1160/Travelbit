095b051ee566863cb374a3f060ed8c3cb5abb1b9
nadineb <nadineb@umich.edu>
2020-05-21 18:52:04 -0700

    [update] route comments and comment out authentication for testing

--- project3/routes/api/country.js
+++ project3/routes/api/country.js
@@ -3,16 +3,17 @@ const countryController = require("../../controllers/countryController");
 
 
 // Matches with "/api/country"
-router.use(function(req, res, next) {
-    if (!req.headers.authorization) {
-      return res.status(403).json({ error: 'No credentials sent!' });
-    }
-    next();
-  }).route("/")
+// router.use(function(req, res, next) {
+//     if (!req.headers.authorization) {
+//       return res.status(403).json({ error: 'No credentials sent!' });
+//     }
+//     next();
+//   }).route("/")
+router.route("/")
     .get(countryController.findAll)
     .post(countryController.create);
 
-// Matches with "api/country/:id"
+// Matches with "api/user/:userId/country/:countryId"
 router.route("/:id")
     .get(countryController.findById)
     .put(countryController.update)
