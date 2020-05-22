5ed3abf8fe9a93810de0c7eda03a73e3355216c0
nadineb <nadineb@umich.edu>
2020-05-21 18:45:07 -0700

    [add] logout and styling

--- project3/client/src/components/Profile/index.js
+++ project3/client/src/components/Profile/index.js
@@ -4,11 +4,23 @@ import { auth } from "../../firebase";
 
 const ProfilePage = () => {
   const { user } = useUserContext();
-  const {name, email} = user;
+  const {displayName, email} = user;
   console.log(user)
+
+  const logoutHandler = () =>  {
+    auth.signOut().then(function() {
+      // Sign-out successful.
+      console.log("Logged out")
+      window.location.href = "/"
+    }).catch(error => {
+      // An error happened.
+      console.log(error)
+    });
+  }
+
   return (
     <div className = "mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
-      <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
+      <div className="flex border rounded flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
         <div
           style={{
             background:
@@ -17,14 +29,14 @@ const ProfilePage = () => {
             height: "200px",
             width: "200px"
           }}
-          className="border border-blue-300"
+          className="border border-blue-300 rounded"
         ></div>
         <div className = "md:pl-4">
-        <h2 className = "text-2xl font-semibold">{name}</h2>
-        <h3 className = "italic">{email}</h3>
+        <h2 className = "text-2xl font-semibold">{displayName}</h2>
+        <h3 className = "italic pt-1">{email}</h3>
         </div>
       </div>
-      <button className = "w-full py-3 bg-red-600 mt-4 text-white">Sign out</button>
+      <button onClick={logoutHandler} className = "w-full rounded py-3 bg-teal-600 mt-4 text-white">Sign out</button>
     </div>
   ) 
 };
