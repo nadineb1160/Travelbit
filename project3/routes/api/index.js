35627febc5ede0487de1285ff828505231115bd4
nadineb <nadineb@umich.edu>
2020-05-21 21:53:52 -0700

    [comment] country routes, now linked with user

--- project3/routes/api/index.js
+++ project3/routes/api/index.js
@@ -1,13 +1,13 @@
 const router = require("express"). Router();
 const userRoutes = require("./user");
-const countryRoutes = require("./country");
+// const countryRoutes = require("./country");
 const cityRoutes = require("./city");
 
 // User routes
 router.use("/user", userRoutes);
 
 // Country routes
-router.use("/country", countryRoutes);
+// router.use("/country", countryRoutes);
 
 // City routes
 router.use("/city", cityRoutes);
