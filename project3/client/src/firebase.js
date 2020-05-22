d4d908aaf12e2240029e93af0487a4d713bf935a
nadineb <nadineb@umich.edu>
2020-05-21 18:44:48 -0700

    [update]  with uid

--- project3/client/src/firebase.js
+++ project3/client/src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import { UserProvider } from "./state/UserContext";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBI_TADYflj_M4qeZD0XslBFWqo46zpYYg",
@@ -18,26 +17,26 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // user
-    }
-})
 
 
-export const generateUserDocument = async(user, additionalData) => {
+export const generateUserDocument = async (user, additionalData) => {
     if (!user) return;
+
     const userRef = firestore.doc(`users/${user.uid}`);
     const snapshot = await userRef.get();
+
     if (!snapshot.exists) {
-        const { email, displayName } = user;
+        const { email, displayName, uid } = user;
+    
         try {
             await userRef.set({
-                displayName,
+                uid,
                 email,
+                displayName,
                 ...additionalData
-                
+
             });
+
         } catch (error) {
             console.error("Error creating user document", error)
         }
@@ -48,16 +47,13 @@ export const generateUserDocument = async(user, additionalData) => {
 const getUserDocument = async uid => {
     if (!uid) return null;
     try {
-      const userDocument = await firestore.doc(`users/${uid}`).get();
-  
-      return {
-        uid,
-        ...userDocument.data()
-      };
+        const userDocument = await firestore.doc(`users/${uid}`).get();
+        return {
+            uid,
+            ...userDocument.data()
+        };
     } catch (error) {
-      console.error("Error fetching user", error);
+        console.error("Error fetching user", error);
     }
 }
 
-
-// export default fireBaseApp;
