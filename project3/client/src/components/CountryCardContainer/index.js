b21cead8088faa2790a2ed59d92c3df95ec9e35c
nadineb <nadineb@umich.edu>
2020-05-22 21:03:50 -0700

    [add] country card container

---
+++ project3/client/src/components/CountryCardContainer/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import CountryCard from '../CountryCard';
+
+
+const CountryCardContainer = ({ cards }) => {
+
+    return (
+        <div className="flex flex-wrap justify-center p-6 m-2">
+            {cards.map((card) => <CountryCard key={card.id} card={card} />)}
+        </div>
+    );
+}
+
+export default CountryCardContainer;
