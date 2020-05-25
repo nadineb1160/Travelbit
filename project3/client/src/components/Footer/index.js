6eede1d49ba0d77f0c25ddf8f23202fbe7ad52d3
nadineb <nadineb@umich.edu>
2020-05-24 22:13:28 -0700

    [add] new footer

--- project3/client/src/components/Footer/index.js
+++ project3/client/src/components/Footer/index.js
@@ -3,15 +3,57 @@ import React from "react";
 function Footer() {
   return (
 
-    <nav className="navbar fixed-bottom navbar-dark bg-dark">
-    
-      <h1 className="text-white">Made with love</h1>
-      <svg className="bi bi-heart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd" />
-      </svg>
-   
-
-    </nav>
+    <footer class="bg-teal-600 bg-opacity-50 object-bottom">
+      <div class="container mx-auto px-4 pt-10 pb-6">
+        <div class="flex flex-wrap">
+          <div class="w-full md:w-1/4 text-center md:text-left">
+            <h1 className="font-semibold text-4xl tracking-tight text-white elite bg-teal-600 p-6 mb-4">Travel | <span className="text-gray-900">Bit</span></h1>
+          </div>
+          <div class="w-full md:w-1/4 text-center md:text-left popppins">
+            <h5 class="uppercase mb-3 font-bold">Links</h5>
+            <ul class="mb-4">
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">FAQ</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Help</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Support</a>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full md:w-1/4 text-center md:text-left popppins">
+            <h5 class="uppercase mb-3 font-bold">Social</h5>
+            <ul class="mb-4">
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Facebook</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Linkedin</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Twitter</a>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full md:w-1/4 text-center md:text-left poppins">
+            <h5 class="uppercase mb-3 font-bold">Company</h5>
+            <ul class="mb-4">
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Blog</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">About Us</a>
+              </li>
+              <li class="mt-2">
+                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
 
   );
 }
