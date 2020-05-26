c6c2f7cae5e4bb6d3577915857e9b0b242b5f6ca
nadineb <nadineb@umich.edu>
2020-05-25 22:23:31 -0700

    [rmv] methods not used

--- project3/controllers/userController.js
+++ project3/controllers/userController.js
@@ -18,24 +18,6 @@ module.exports = {
         });
         
     },
-    // findById: (req, res) => {
-    //     db.User
-    //     .findOne({
-    //         where: {
-    //             id: req.param.userId
-    //         },
-    //         include: [{
-    //             model: db.Country
-    //         }]
-    //     }).then(user => {
-    //         console.log("found user by id")
-    //         console.log(user)
-    //         res.json(user);
-    //     }).catch(err => {
-    //         console.log(err);
-    //         res.send('No data found')
-    //     });
-    // },
     findByUid: (req, res) => {
         console.log(req.params.uid)
         db.User
@@ -80,50 +62,8 @@ module.exports = {
             console.log(err);
             res.send("Failed to update");
         });
-    },
+    }
 
-    // Check Existing User at Signup
-    // checkExisting: (req, res) => {
-    //     const { email, password } = req.body;
-
-    //     // Check all fields
-    //     if (!email || !password) {
-    //         return res.status(400).json({ msg: "Please enter all fields"});
-    //     }
-
-    //     db.User
-    //     .findOne({
-    //         where: {
-    //             email: email
-    //         }
-    //     }).then(user => {
-    //         if (user) return res.status(400).json( {msg: "User already exists"});
-
-    //         const newUser = new User({
-    //             name,
-    //             email,
-    //             password
-    //         })
-
-    //         // Create hash and salt
-    //         bcrypt.genSalt(10, (err, salt) => {
-    //             bcrypt.hash(newUser.password, salt, (err, hash) => {
-    //                 if (err) throw err;
-    //                 newUser.password = hash;
-    //                 newUser.save()
-    //                 .then(user => {
-    //                     res.json({
-    //                         user: {
-    //                             id: user.id,
-    //                             name: user.name,
-    //                             email: user.email
-    //                         }
-    //                     })
-    //                 })
-    //             })
-    //         })
-    //     })
-    // }
 }
 
 
