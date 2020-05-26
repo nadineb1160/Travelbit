9d5e9bba6cafabe60ae0f137378ee736a4436e11
nadineb <nadineb@umich.edu>
2020-05-25 22:22:29 -0700

    [add] routes in index

--- project3/routes/api/index.js
+++ project3/routes/api/index.js
@@ -1,16 +1,24 @@
 const router = require("express"). Router();
 const userRoutes = require("./user");
-// const countryRoutes = require("./country");
+const countryRoutes = require("./country");
+const stateRoutes = require("./state");
 const cityRoutes = require("./city");
+const tripRoutes = require("./trip");
 
 // User routes
 router.use("/user", userRoutes);
 
 // Country routes
-// router.use("/country", countryRoutes);
+router.use("/country", countryRoutes);
+
+// State routes
+router.use("/state", stateRoutes);
 
 // City routes
 router.use("/city", cityRoutes);
+
+// Trip routes
+router.use("/trip", tripRoutes);
 
 
 module.exports = router;
