a23233c9872399a91243fedf92107eb1a0f5db56
nadineb <nadineb@umich.edu>
2020-05-25 22:27:43 -0700

    [add] className

--- project3/client/src/components/Footer/index.js
+++ project3/client/src/components/Footer/index.js
@@ -3,51 +3,51 @@ import React from "react";
 function Footer() {
   return (
 
-    <footer class="bg-teal-600 bg-opacity-50 object-bottom">
-      <div class="container mx-auto px-4 pt-10 pb-6">
-        <div class="flex flex-wrap">
-          <div class="w-full md:w-1/4 text-center md:text-left">
-            <h1 className="font-semibold text-4xl tracking-tight text-white elite bg-teal-600 p-6 mb-4">Travel | <span className="text-gray-900">Bit</span></h1>
+    <footer className="bg-teal-600 bg-opacity-50 object-bottom">
+      <div className="container mx-auto px-4 pt-10 pb-6">
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/4 text-center md:text-left">
+            <h1 className="font-semibold text-4xl tracking-tight text-white elite bg-teal-600 p-6 mb-4 rounded">Travel | <span className="text-gray-900">Bit</span></h1>
           </div>
-          <div class="w-full md:w-1/4 text-center md:text-left popppins">
-            <h5 class="uppercase mb-3 font-bold">Links</h5>
-            <ul class="mb-4">
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">FAQ</a>
+          <div className="w-full md:w-1/4 text-center md:text-left popppins">
+            <h5 className="uppercase mb-3 font-bold">Links</h5>
+            <ul className="mb-4">
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">FAQ</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Help</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Help</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Support</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Support</a>
               </li>
             </ul>
           </div>
-          <div class="w-full md:w-1/4 text-center md:text-left popppins">
-            <h5 class="uppercase mb-3 font-bold">Social</h5>
-            <ul class="mb-4">
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Facebook</a>
+          <div className="w-full md:w-1/4 text-center md:text-left popppins">
+            <h5 className="uppercase mb-3 font-bold">Social</h5>
+            <ul className="mb-4">
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Facebook</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Linkedin</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Linkedin</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Twitter</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Twitter</a>
               </li>
             </ul>
           </div>
-          <div class="w-full md:w-1/4 text-center md:text-left poppins">
-            <h5 class="uppercase mb-3 font-bold">Company</h5>
-            <ul class="mb-4">
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Blog</a>
+          <div className="w-full md:w-1/4 text-center md:text-left poppins">
+            <h5 className="uppercase mb-3 font-bold">Company</h5>
+            <ul className="mb-4">
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Blog</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">About Us</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">About Us</a>
               </li>
-              <li class="mt-2">
-                <a href="#" class="hover:underline text-gray-600 hover:text-teal-600">Contact</a>
+              <li className="mt-2">
+                <a href="#" className="hover:underline text-gray-600 hover:text-teal-600">Contact</a>
               </li>
             </ul>
           </div>
