d78065967e8f99903ee5e0a702411c290849e81b
nadineb <nadineb@umich.edu>
2020-05-25 22:25:15 -0700

    [update] state page

--- project3/client/src/pages/State.js
+++ project3/client/src/pages/State.js
@@ -1,61 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import AddState from "../components/AddState";
 import StateCardContainer from '../components/StateCardContainer';
-import { useUserContext } from "../state/UserContext.js"
+import { useUserContext } from "../state/UserContext.js";
 import API from "../utils/API";
 
-const State = ({countryId}) => {
+const State = () => {
     console.log("Hello made it to state")
     const { user } = useUserContext();
+    const { countryId } = useParams();
 
     const [displayCards, setDisplayCards] = useState([]);
     // const [selected, setSelected] = useState({});
     const [loading, setLoading] = useState(false);
+    const [adding, setAdding] = useState(false);
 
     console.log(countryId)
+    // console.log(displayCards)
 
     useEffect(() => {
         setLoading(true);
         console.log(user)
-        API.getUserByUid(user.uid)
-        .then((id) => {
-            let userId = id.data.id;
-            console.log(userId);
-    
-            API.getStates(userId, countryId).then((states) => {
-                console.log("States")
-                console.log(states.data);
+       
+        API.getStates(countryId).then((states) => {
+            console.log("States")
+            
+            let stateData = states.data;
+            console.log(stateData);
 
-                let stateData = states.data;
-
-                setDisplayCards(stateData);
-                setLoading(false);
-            })
-        });
+            setDisplayCards(stateData);
+            setLoading(false);
+        })
         
     }, []);
 
     const addStateHandler = (event) => {
-        return (
-            window.location.href = "/addState"
-        )
+        
+        setAdding(true);
+        
     }
 
+    if (loading) return <span>Loading...</span>
 
-    return (
+    return adding ? (<AddState countryId={countryId}/>) : (
+
         <div>
             <div className="flex justify-center mt-4">
-                {/* <h2 className="text-center text-xl font-bold">{name}Countries</h2>
-                <br/> */}
+        
                 <button onClick={addStateHandler} className="shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                     Add State
                 </button>
             </div>
-           
-            {/* <StateCardContainer
+            
+            <StateCardContainer
                 cards={displayCards}
-            /> */}
+            />
         </div>
     )
+    
 }
 
 export default State;
