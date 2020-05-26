0a2a515b297f31b17727aa98903468bbe60f37ea
nadineb <nadineb@umich.edu>
2020-05-25 22:29:58 -0700

    [update] CoountryCard with rerouting for US

--- project3/client/src/components/CountryCard/index.js
+++ project3/client/src/components/CountryCard/index.js
@@ -1,39 +1,40 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import State from "../../pages/State";
 import Home from "../../pages/Home";
+import City from "../../pages/CityFromCountry";
 // import City from "../../pages/City";
 // import { useState } from "react";
 
 function CountryCard({ card }) {
+    const history = useHistory();
 
     // const [selectedCountryId, setSelectedCountryId] = useState();
 
     const clickCardHandler = (event) => {
         event.preventDefault();
         // console.log(event.currentTarget)
-        let cardId = event.currentTarget.getAttribute('value');
-        let cardName = event.currentTarget.getAttribute('name');
-        console.log(cardId)
-        console.log(cardName)
+        let countryId = event.currentTarget.getAttribute('value');
+        let countryName = event.currentTarget.getAttribute('name');
+        console.log(countryId)
+        console.log(countryName)
 
-        if (cardName === "United States of America" || cardName === "USA" || cardName === "United States") {
+        if (countryName === "United States of America" || countryName === "USA" || countryName === "United States") {
             console.log("states");
             return (
-                // <State countryId={cardId}></State>
-                window.location.href = "/state"
+                history.push(`/country/${countryId}/state`)
                 
             )
         } else {
             return (
-                // <City></City>
-                window.location.href = "/city"
+                history.push(`/country/${countryId}/city`)
             )
         }
 
     }
 
     return (
-        <div onClick={clickCardHandler} className="max-w-sm border rounded overflow-hidden shadow-lg m-3"  value={card.id} name={card.countryName}>
+        <div onClick={(event) => clickCardHandler(event)} className="max-w-sm border rounded overflow-hidden shadow-lg m-3 bg-white"  value={card.id} name={card.countryName}>
             <img className="w-full p-3" src="https://cdn.pixabay.com/photo/2016/01/09/18/27/old-1130731__480.jpg" alt="Travel Map"/>
             <div className="px-3 py-2">
                 <h1 className="font-bold text-5xl mb-2 marker text-center">{card.countryName}</h1>
