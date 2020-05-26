7adb29ff58eb52d516797b52f21394b03bf8bc04
nadineb <nadineb@umich.edu>
2020-05-25 22:27:32 -0700

    [add] new client routes

--- project3/client/src/components/Router/index.js
+++ project3/client/src/components/Router/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import About from "../../pages/About";
-import Home from "../../pages/Home";
+
+// Components
 import SignUp from "../SignUp";
 import SignIn from "../SignIn";
 import PasswordReset from "../PasswordReset";
 import AddCountry from "../AddCountry";
 import AddCity from "../AddCity";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import Wrapper from "../Wrapper";
+
+// Pages
+import About from "../../pages/About";
+import Home from "../../pages/Home";
 import Profile from "../../pages/Profile";
 import Travel from "../../pages/Travel";
 import State from "../../pages/State";
-import City from "../../pages/City";
-import Navbar from "../Navbar/index";
-import Footer from "../Footer/index";
-import Wrapper from "../Wrapper/index";
+import CityFromCountry from "../../pages/CityFromCountry";
+import CityFromState from "../../pages/CityFromState";
+import Trip from "../../pages/Trip";
+
+// User State
 import { useUserContext } from "../../state/UserContext.js"
 
 function AuthenticatedRoutes() {
@@ -43,8 +51,10 @@ function Router() {
                         <Route exact path="/profile" component={Profile} />
                         <Route exact path="/addCountry" component={AddCountry} />
                         <Route exact path="/addCity" component={AddCity} />
-                        <Route exact path="/state" component={State} />
-                        <Route exact path="/city" component={City} />
+                        <Route exact path="/country/:countryId/state" component={State} />
+                        <Route exact path="/country/:countryId/city" component={CityFromCountry} />
+                        <Route exact path="/state/:stateId/city" component={CityFromState} />
+                        <Route exact path="/city/:cityId/trip" component={Trip} />
                         {/* <Route exact path="/signup" component={SignUp} />
                     <Route exact path="/signin" component={SignIn} /> */}
                         {/* <Route component={NoMatch} /> */}
