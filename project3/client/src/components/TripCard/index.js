76132fb56187f44d17d013ffea6376ba89a17f19
nadineb <nadineb@umich.edu>
2020-05-27 21:51:57 -0700

    [add] start and end date

--- project3/client/src/components/TripCard/index.js
+++ project3/client/src/components/TripCard/index.js
@@ -5,6 +5,15 @@ import { useHistory } from "react-router-dom";
 function TripCard({ card }) {
     const history = useHistory();
 
+    console.log(card)
+
+    // Date Format - MM/DD/YYYY
+    let tripStart = card.startDate.split(" ").slice(1, 4).join("/");
+    let tripEnd = card.endDate.split(" ").slice(1, 4).join("/");
+
+    console.log(tripStart)
+    console.log(tripEnd)
+
     const clickCardHandler = (event) => {
         event.preventDefault();
        
@@ -23,6 +32,7 @@ function TripCard({ card }) {
             <img className="w-full p-3" src="https://cdn.pixabay.com/photo/2016/01/09/18/27/old-1130731__480.jpg" alt="Travel Map"/>
             <div className="px-3 py-2">
                 <h1 className="font-bold text-5xl mb-2 marker text-center">{card.tripName}</h1>
+                <p className="font-bold text-xl mb-2 text-center">{tripStart} - {tripEnd}</p>
             </div>
         
         </div>
