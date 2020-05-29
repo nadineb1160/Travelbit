35694679211a0ca877a9d785d7f746ebf165c5fa
nadineb <nadineb@umich.edu>
2020-05-29 09:22:35 -0700

    [add] flex-grow to wrapper

--- project3/client/src/components/Wrapper/index.js
+++ project3/client/src/components/Wrapper/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Wrapper = ({children}) => {
     return (
-        <div className="">
+        <div className="flex-grow">
            {children}
         </div>
     )
