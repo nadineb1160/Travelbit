da546836c647eee6ffdc40e1728a6ebcda0e3658
nadineb <nadineb@umich.edu>
2020-05-28 22:17:06 -0700

    [update]

--- project3/client/src/components/CountryCardContainer/index.js
+++ project3/client/src/components/CountryCardContainer/index.js
@@ -3,7 +3,7 @@ import CountryCard from '../CountryCard';
 
 
 const CountryCardContainer = ({ cards }) => {
-    
+
     return (
         <div className="flex flex-wrap justify-center p-6 m-2">
             {cards.map((card) => <CountryCard key={card.id} card={card} />)}
