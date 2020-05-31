440a3de89853803987297795473f70b3c13963af
nadineb <nadineb@umich.edu>
2020-05-30 18:56:06 -0700

    [add] api call and search card mapping

--- client/src/components/SearchCardContainer/index.js
+++ client/src/components/SearchCardContainer/index.js
@@ -1,19 +1,19 @@
 import React, {useState, useEffect} from "react";
 import ImageSearch from '../ImageSearch';
-import Card from '../Card';
+import SearchCard from '../SearchCard';
 
 const SearchCardContainer = () => {
     const [images, setImages] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
-    // useEffect(() => {
-    //     fetch(`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setImages(data.hits);
-    //         })
-    //         .catch(err => console.log(err))
-    // }, [searchTerm]);
+    useEffect(() => {
+        fetch(`https://pixabay.com/api/?key=16522680-31d5f48c0d4b1e06839be958d&q=${searchTerm}&image_type=photo&pretty=true`)
+            .then(res => res.json())
+            .then(data => {
+                setImages(data.hits);
+            })
+            .catch(err => console.log(err))
+    }, [searchTerm]);
 
     return (
         <div className="container mx-auto">
@@ -21,15 +21,11 @@ const SearchCardContainer = () => {
 
             {/* {!isLoading && images.length === 0 && <h1>No Images Found</h1>} */}
             
-            <div className="grid grid-cols-4 gap-4">
-                {/* {images.map(image =>
-                    <Card key={image.id} image={image}/>
-                )} */}
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-               
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
+                {images.map(image =>
+                    <SearchCard key={image.id} image={image}/>
+                )}
+                
             </div>
         </div>
     )
