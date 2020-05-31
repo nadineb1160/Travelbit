234500a64356e0c8e17b6f780d08c516a9a46a92
nadineb <nadineb@umich.edu>
2020-05-30 18:55:13 -0700

    [update] search card

--- client/src/components/SearchCard/index.js
+++ client/src/components/SearchCard/index.js
@@ -4,22 +4,10 @@ function SearchCard({ image }) {
     // const tags = image.tags.split(',');
 
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="rounded shadow-lg">
             {/* Change image src={image.webformatURL} */}
-            <img src="https://source.unsplash.com/random" alt="random" className="w-full" />
-            <div className="px-6 py-4">
-                <div className="font-bold text-purple-500 text-xl mb-2">
-                    Country Name
-                 </div>
-            </div>
+            <img src={image.webformatURL} alt="random" className="w-full" />
 
-            {/* <div className="px-6 py-4">
-                {tags.map((tag, index) => (
-                    <span key={index}className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                        #{tag}
-                    </span>
-                ))}
-            </div> */}
         </div>
 
     );
