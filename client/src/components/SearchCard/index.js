dade1b4479d67c80d7f489bd3805e833de469ddf
nadineb <nadineb@umich.edu>
2020-05-31 22:47:43 -0700

    [rmv] rubbish

--- client/src/components/SearchCard/index.js
+++ client/src/components/SearchCard/index.js
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from "react";
 
 function SearchCard({ image }) {
-    // const tags = image.tags.split(',');
 
     return (
         <div className="rounded shadow-lg">
-            {/* Change image src={image.webformatURL} */}
             <img src={image.webformatURL} alt="random" className="w-full" />
 
         </div>
