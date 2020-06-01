f43f8b879f44408e515d89cf9c92d11773a224a6
nadineb <nadineb@umich.edu>
2020-05-31 18:05:36 -0700

    [update] placeholder

--- client/src/components/ImageSearch/index.js
+++ client/src/components/ImageSearch/index.js
@@ -11,7 +11,7 @@ const ImageSearch = ({ searchText }) => {
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
             <form className="w-full max-w-sm" onSubmit={onSubmit}>
                 <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" onChange={e => setSearchTerm(e.target.value)} type="text" placeholder="Jane Doe" aria-label="Full name" />
+                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" onChange={e => setSearchTerm(e.target.value)} type="text" placeholder="Berlin" aria-label="search" />
                     <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
                     </button>
